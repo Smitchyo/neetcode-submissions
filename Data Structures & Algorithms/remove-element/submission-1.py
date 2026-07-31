c13b765408df94a7383384560bf1ff865512bee5
Sam Bigwood <sam.a.bigwood@gmail.com>
2026-07-31 19:23:37 +0100

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/remove-element/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        ans = []
+        for i in range(len(nums)):
+            if nums[i] != val:
+                ans.append(nums[i])
+        nums = ans
+        return len(nums)
